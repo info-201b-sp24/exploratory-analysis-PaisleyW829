6dfb8c0ab9a913f720532a77d365acfb70c38f10
PaisleyW829 <yuewew2@uw.edu>
2024-05-17 15:38:08 -0700

    add chart1 file

---
+++ chart1.R
@@ -0,0 +1,17 @@
+```{r}
+library(ggplot2)
+library(readr)
+```
+
+```{r}
+BR <- read_csv("U.S._and_State_Trends_on_Teen_Births.csv")
+```
+
+```{r}
+ggplot(BR, aes(x = Year, y = `State Rate`, color = State)) +
+  geom_line() +
+  labs(title = "Trends in Teenage Pregnancy Rates by State", x = "Year", y = "Teenage Pregnancy Rate") +
+  theme_minimal()
+```
+
+- For the first research question, we chose to use a line graph, we believe this would be appropriate because it can clearly show changes over time across different states. This will allow us to identify trends and variations in teenage pregnancy rates over approximately the past decade.
