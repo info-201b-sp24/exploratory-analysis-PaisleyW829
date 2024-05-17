a6dfee08ea0f9fdebc6ca4076267130fb5f493bb
PaisleyW829 <yuewew2@uw.edu>
2024-05-17 15:48:42 -0700

    add chart1 file

--- chart1.R
+++ chart1.R
@@ -1,5 +1,6 @@
 ```{r}
 library(ggplot2)
+library(dplyr)
 ```
 
 ```{r}
@@ -13,4 +14,4 @@ ggplot(BR, aes(x = Year, y = `State Rate`, color = State)) +
   theme_minimal()
 ```
 
-- For the first research question, we chose to use a line graph, we believe this would be appropriate because it can clearly show changes over time across different states. This will allow us to identify trends and variations in teenage pregnancy rates over approximately the past decade.
+- For the first research question, we chose to use the line graph, we believe this would be appropriate because it can clearly show changes over time across different states. This will allow us to identify trends and variations in teenage pregnancy rates over approximately the past decade.
