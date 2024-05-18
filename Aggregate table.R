6d42592ed7e00c40717459a1d56e0a351b353ca2
TippiFang <siyufang@uw.edu>
2024-05-17 18:00:46 -0700

    table with description

--- Aggregate table.R
+++ Aggregate table.R
@@ -36,3 +36,15 @@ us_birth_rate_yearly <- teen_births_data %>%
   ) %>%
   arrange(Year)
 
+##One of these two tables is the annual average teen birth rate and the total 
+## number of teen births in each state, while the other is a more 
+## summarized table that contains the average teen birth rate and the
+##total number of teen births in the United States for each year.
+## The state table can help us more clearly see the annual data of teen birth in 
+##different states, facilitate us to make a clearer comparison,and reveal
+## the possible reasons behind the data changes, such as which state may implement
+## relevant policies and which state does not. 
+## The US birth table reveals the overall data of this country. 
+##Through the comparison between different years, we can guess 
+##whether the United States has taken actions control this behavior and whether
+##the teenagers' sexual awareness has increased.
