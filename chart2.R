5c3aefefb071aa07558ea20856144c85cbe0fc9e
PaisleyW829 <yuewew2@uw.edu>
2024-05-17 20:32:49 -0700

    add chart2 file

--- chart2.R
+++ chart2.R
@@ -16,7 +16,5 @@ ggplot(BR, aes(x = Year, y = `U.S. Birth Rate`)) +
   theme_minimal()
 ```
 
-- We decided to choose the line and point plot for visualizing the U.S. birth rate over the years primarily because it could effectively and clearly illustrate the trends over time. Line and point plots are excellent for time series data, allowing clear visualization of changes in the birth rate—whether it's rising, declining, or remaining stable. This type of plot provides a continuous view of data, highlighting relationships and movements over the years, which is crucial for understanding demographic dynamics. It can pinpoint trends, detect volatility, and identify anomalies, offering a comprehensive overview of how external factors might influence birth rates. Thus, a line plot not only displays data succinctly but also facilitates easy interpretation of historical and current patterns in birth rates, making it an invaluable tool for researchers and policymakers.
-
-
+- We decided to choose the line and point plot for visualizing the U.S. birth rate over the years primarily because it could effectively and clearly illustrate the trends over time. Line and point plots are excellent for time series data, allowing clear visualization of changes in the birth rate—whether it's rising, declining, or remaining stable. This type of plot provides a continuous view of data, highlighting relationships and movements over the years, which is crucial for understanding demographic dynamics. It can showcase the trends, detect volatility, and identify anomalies, offering a comprehensive overview of how external factors might influence birth rates. 
 
